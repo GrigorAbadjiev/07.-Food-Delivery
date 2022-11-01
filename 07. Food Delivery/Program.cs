1f2f10ed2f9b80ed911e0e8b11547905ac622b57
User <User@DESKTOP-ESP2JT1>
2022-11-01 14:27:21 +0200

    Add project files.

---
+++ 07. Food Delivery/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _07._Food_Delivery
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // input
+            int chikenMenuCount = int.Parse(Console.ReadLine());
+            int fishMenuCount = int.Parse(Console.ReadLine());
+            int vegeterianMenuCount = int.Parse(Console.ReadLine());
+            // calculate
+            double chikenMenuPrice = chikenMenuCount * 10.35;
+            double fishMenuPrice = fishMenuCount * 12.40;
+            double vegeterianMenuPrice = vegeterianMenuCount * 8.15;
+            double totalMenuPrice = chikenMenuPrice + fishMenuPrice + vegeterianMenuPrice;
+            double dessertPrice = totalMenuPrice * 0.2;
+            // output
+            Console.WriteLine(totalMenuPrice + dessertPrice + 2.50);
+
+
+        }
+    }
+}
